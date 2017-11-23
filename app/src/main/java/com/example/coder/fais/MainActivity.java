343aa4dd37b3e335811a3f9451f5395205216719
Mahadev Sapkota <madhav.sapkota@gmail.com>
2017-11-23 17:04:53 -0500

    Linked category, subcategory and descriptions

--- app/src/main/java/com/example/coder/fais/MainActivity.java
+++ app/src/main/java/com/example/coder/fais/MainActivity.java
@@ -5,35 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ListView;
 import android.widget.AdapterView;
+import android.widget.ExpandableListView;
 import android.widget.Toast;
 
-import com.example.coder.fais.models.Categories;
+import com.example.coder.fais.models.CategoryAdapter;
 import com.example.coder.fais.utils.Constants;
 import com.example.coder.fais.utils.FireBase;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    ListView catergoryListView;
+   /* ListView catergoryListView;
     CustomizedListAdapter listAdapter;
     ArrayList<Categories> categoriesList = new ArrayList<Categories>();
     ChildEventListener mChildEventListener;
-    DatabaseReference myRef;
+    DatabaseReference myRef;*/
+   private CategoryAdapter categoryAdapter;
+    private ExpandableListView expLV;
+    private ChildEventListener mChildEventListener;
+    private Map<String, ArrayList<String>> mapChild;
+    private DatabaseReference myRef;
+    private DatabaseReference subRef;
+    private ArrayList<String> listCategories = new ArrayList<String>();
+    private ArrayList<Integer> subCategory = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        catergoryListView = (ListView) findViewById(R.id.lv_category_list);
+        getList();
+        /*catergoryListView = (ListView) findViewById(R.id.lv_category_list);
         listAdapter = new CustomizedListAdapter(this, categoriesList);
         catergoryListView.setAdapter(listAdapter);
         catergoryListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -87,10 +96,137 @@ public class MainActivity extends AppCompatActivity {
         };
 
         myRef.addChildEventListener(mChildEventListener);
+*/
+    }
+
+
+    private void getList(){
+
+        expLV= (ExpandableListView)findViewById(R.id.expLV);
+        listCategories = new ArrayList<>();
+        mapChild = new HashMap<>();
+
+
+
+        expLV.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+            int previousItem = -1;
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                if (groupPosition != previousItem)
+                    expLV.collapseGroup(previousItem);
+                previousItem = groupPosition;
+            }
+        });
+
+        categoryAdapter = new CategoryAdapter(listCategories,mapChild,MainActivity.this);
+        expLV.setAdapter(categoryAdapter);
+
+
+        myRef = FireBase.getInstance().getFireBaseReference(Constants.FIRBASE_Category_DATA);
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String key = dataSnapshot.getKey();
+
+                //if(messageSnapshot.getKey().equals("CategoryId")) {
+                subRef = FireBase.getInstance().getFireBaseReference(Constants.FIRBASE_SubCategory_DATA);
+                final String categoryName = dataSnapshot.child("Name").getValue().toString();
+                listCategories.add(categoryName);
+                Query query=subRef.orderByChild("CategoryId").equalTo(Integer.valueOf(dataSnapshot.child("CategoryId").getValue().toString()));
+                query.addChildEventListener(new ChildEventListener(){
+
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        if(mapChild.containsKey(categoryName)){
+                            mapChild.get(categoryName).add(dataSnapshot.child("Name").getValue().toString());
+                        }else{
+                            ArrayList<String> subcategoryList = new ArrayList<>();
+                            subcategoryList.add(dataSnapshot.child("Name").getValue().toString());
+                            subCategory.add(Integer.valueOf( dataSnapshot.child("SubCategoryId").getValue().toString()));
+                            mapChild.put(categoryName, subcategoryList);
+                        }
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+
+                categoryAdapter.notifyDataSetInvalidated();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+
+        };
+
+        myRef.addChildEventListener(mChildEventListener);
+
+        expLV.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                String selected = (String) categoryAdapter.getChild(
+                        groupPosition, childPosition);
+
+                Log.d("selected..........",selected);
+
+                Intent intent = new Intent(MainActivity.this, FaisTabActivity.class);
+                intent.putExtra("SubCategoryId",selected);
+                startActivity(intent);
+               /* Toast.makeText(getBaseContext(), selected, Toast.LENGTH_LONG)
+                        .show();*/
+
+                return true;
+            }
+        });
+
+
+
 
     }
 
-    }
+}
+
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/coder/fais/models/CategoryAdapter.java
@@ -0,0 +1,111 @@
+package com.example.coder.fais.models;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.DataSetObserver;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.coder.fais.FaisTabActivity;
+import com.example.coder.fais.R;
+import com.example.coder.fais.SubCategoryActivity;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by jaineeta on 11/21/17.
+ */
+
+public class CategoryAdapter extends BaseExpandableListAdapter
+        {
+
+    private ArrayList<String> listCategories;
+    private Map<String, ArrayList<String>> mapChild;
+    private Context context;
+    Integer img=R.drawable.cpr;
+
+    public CategoryAdapter(ArrayList<String> listCategories, Map<String, ArrayList<String>> mapChild, Context context) {
+        this.listCategories = listCategories;
+        this.mapChild = mapChild;
+        this.context = context;
+        this.img=img;
+    }
+
+
+
+    @Override
+    public int getGroupCount() {
+        return listCategories.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return mapChild.get(listCategories.get(groupPosition)).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return listCategories.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return mapChild.get(listCategories.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+
+        String totalCategories=(String)getGroup(groupPosition);
+        convertView = LayoutInflater.from(context).inflate(R.layout.category_group,null);
+        ImageView image= (ImageView) convertView.findViewById(R.id.catimage);
+        image.setImageResource(R.drawable.cpr);
+        TextView tvGroup = (TextView) convertView.findViewById(R.id.catgroup);
+
+        tvGroup.setText(totalCategories);
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+        String item = (String)getChild(groupPosition,childPosition);
+        convertView = LayoutInflater.from(context).inflate(R.layout.subcategory_group,null);
+        TextView tvGroup = (TextView) convertView.findViewById(R.id.subcat);
+        tvGroup.setText(item);
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+
+
+        /*Intent intent = new Intent(this.context,FaisTabActivity.class);
+
+
+        int i=mapChild.get(listCategories.get(groupPosition)).get(childPosition);
+        intent.putExtra("SubCategoryId",i);
+        startActivity(intent);*/
+        return true;
+    }
+}
